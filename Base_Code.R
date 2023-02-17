3639bb642f18974cfc31f3c5da2c9d9a1f7fc935
Evelyn Shi <evelynyshi@gmail.com>
2023-02-16 22:16:04 -0500

    Loop for simulation and results code

--- Base_Code.R
+++ Base_Code.R
@@ -4,9 +4,13 @@
 library(survival)
 library(tidyverse)
 library(gam)
+library(Metrics)
+
+# Number of simulation runs
+num_runs <- 1
 
 # population size
-N <- 20000
+N <- 10000
 
 # study end time
 tau <- 1000
@@ -14,14 +18,23 @@ tau <- 1000
 ################################################################################
 
 # vaccination coverage
-pct_vax <- 0.50
+pct_vax <- 0.5
 
 # vaccine efficacy against infection (1-hazard ratio)
-VE_sph <- 0.5
+VE_s <- 0.85
 # vaccine efficacy against progression to symptoms given infection
-VE_ph <- 0.67
+VE_p <- 0.4
 # vaccine efficacy against progression to severe disease given symptoms
-VE_h <- 0.775
+VE_h <- 0.6
+
+# percent of population that is older
+pct_older <- 0.5
+# hazard ratio relating older age and testing positive
+hr_older_tp <- 1.0
+# hazard ratio relating older age and testing negative
+hr_older_tn <- 1.0
+
+# can further add another layer that allows different vaccination probability
 
 ################################################################################
 
@@ -30,10 +43,15 @@ VE_h <- 0.775
 shape_tp <- 1.2
 rate_tp0 <- 0.01
 
-# baseline probability that an unvaccinated person progresses to symptoms given infection
-prob_dx_tp <- 0.7
-# baseline probability that an unvaccinated person progresses to severe disease given symptoms
-prob_hos_tp <- 0.05
+# baseline probability that a younger unvax person progresses to symptoms given infection
+prob_dx_tp_young <- 0.8
+# baseline probability that a younger unvax person progresses to severe disease given symptoms
+prob_hos_tp_young <- 0.45
+
+# baseline probability that an older unvax person progresses to symptoms given infection
+prob_dx_tp_old <- 0.8
+# baseline probability that an older unvax person progresses to severe disease given symptoms
+prob_hos_tp_old <- 0.45
 
 ################################################################################
 
@@ -42,44 +60,96 @@ prob_hos_tp <- 0.05
 shape_tn <- 0.8
 rate_tn0 <- 0.01
 
-# baseline probability that an unvaccinated person progresses to symptoms given infection
-prob_dx_tp <- 0.5
-# baseline probability that an unvaccinated person progresses to severe disease given symptoms
-prob_hos_tp <- 0.01
+# baseline probability that a younger unvax person progresses to symptoms given infection
+prob_dx_tn_young <- 0.5
+# baseline probability that a younger unvax person progresses to severe disease given symptoms
+prob_hos_tn_young <- 0.05
+
+# baseline probability that an older unvax person progresses to symptoms given infection
+prob_dx_tn_old <- 0.5
+# baseline probability that an older unvax person progresses to severe disease given symptoms
+prob_hos_tn_old <- 0.05
 
 ################################################################################
 
 # probability that a vaccinated person with no symptoms seeks testing
-prob_test_s_vax <- 0.10
+prob_test_1_vax <- 1
 # probability that a vaccinated person with mild symptoms seeks testing
-prob_test_p_vax <- 0.50
+prob_test_2_vax <- 1
 # probability that a vaccinated person with severe symptoms seeks testing
-prob_test_h_vax <- 1.00
+prob_test_3_vax <- 1
 
 # probability that an unvaccinated person with no symptoms seeks testing
-prob_test_s_unvax <- 0.05
+prob_test_1_unvax <- 1
 # probability that an unvaccinated person with mild symptoms seeks testing
-prob_test_p_unvax <- 0.20
+prob_test_2_unvax <- 1
 # probability that an unvaccinated person with severe symptoms seeks testing
-prob_test_h_unvax <- 0.90
+prob_test_3_unvax <- 1
+
+## can further modify this to depend on age
 
 ################################################################################
 
 # run the simulation
-source("TNDfunctions.R")
-View(df)
+set.seed(12345)
 
-split_df <- split(df, df$severity)
+# create dataframe for bias output
+cols <- c("est_VE_s", "est_VE_sp", "est_VE_sph")
+VE_df <- data.frame(matrix(ncol=length(cols),nrow=0))
+colnames(VE_df) <- cols
 
-# fit logistic regression model adjusting for age and calendar time
-logfit <- glm(result ~ vax + age + s(time,3), data=split_df$`1`, family=binomial)
-summary(logfit)
-1-exp(logfit$coefficients[2])
 
-logfit <- glm(result ~ vax + age + s(time,3), data=split_df$`2`, family=binomial)
-summary(logfit)
-1-exp(logfit$coefficients[2])
+for(i in 1:num_runs){
+  source("Functions.R")
+  
+  df_keep <- df[df$keep==1,]
+  df_s <- df_keep
+  df_sp <- df_keep[df_keep$severity==2 | df_keep$severity==3,]
+  df_sph <- df_keep[df_keep$severity==3,]
+  
+  # fit logistic regression model adjusting for age and calendar time
+  # finding VE_s
+  logfit_s <- glm(result ~ vax + age + s(time,3), data=df_s, family=binomial)
+  est_VE_s <- 1-exp(logfit_s$coefficients[2])
+  
+  # finding VE_sp
+  logfit_sp <- glm(result ~ vax + age + s(time,3), data=df_sp, family=binomial)
+  est_VE_sp <- 1-exp(logfit_sp$coefficients[2])
+  
+  # finding VE_sph
+  logfit_sph <- glm(result ~ vax + age + s(time,3), data=df_sph, family=binomial)
+  est_VE_sph <- 1-exp(logfit_sph$coefficients[2])
+  
+  VE_df <- VE_df %>% add_row(est_VE_s = est_VE_s, est_VE_sp = est_VE_sp, 
+                             est_VE_sph = est_VE_sph)
+}
 
-logfit <- glm(result ~ vax + age + s(time,3), data=split_df$`3`, family=binomial)
-summary(logfit)
-1-exp(logfit$coefficients[2])
+view(VE_df)
+
+# Calculating mean biases
+mean_bias_s <- mean(0.85 - VE_df$est_VE_s)
+mean_bias_s
+mean_bias_sp <- mean(0.886 - VE_df$est_VE_sp)
+mean_bias_sp
+mean_bias_sph <- mean(0.91 - VE_df$est_VE_sph)
+mean_bias_sph
+
+# Calculating MSE
+MSE_s <- mean((0.85 - VE_df$est_VE_s)^2)
+MSE_s
+MSE_sp <- mean((0.886 - VE_df$est_VE_sp)^2)
+MSE_sp
+MSE_sph <- mean((0.91 - VE_df$est_VE_sph)^2)
+MSE_sph
+
+# Mean Absolute Error
+MEA_s <- mae(0.85, VE_df$est_VE_s)
+MEA_s
+MEA_sp <- mae(0.886, VE_df$est_VE_sp)
+MEA_sp
+MEA_sph <- mae(0.91, VE_df$est_VE_sph)
+MEA_sph
+
+# Monte Carlo Standard Error
+# average standard error 
+
