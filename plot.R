756bc46060aa8426b6403cbde88e88c24eb6a177
Evelyn Shi <evelynyshi@gmail.com>
2023-02-25 13:38:37 -0500

    Changes in base code and adding plot code

--- plot.R
+++ plot.R
@@ -1,13 +1,9 @@
-## Generate test negative data
-## Allow for different levels of severity
+# plotting
 
 library(survival)
 library(tidyverse)
 library(gam)
 library(Metrics)
-
-# Number of simulation runs
-num_runs <- 10
 
 # population size
 N <- 10000
@@ -23,27 +19,18 @@ pct_vax <- 0.5
 # vaccine efficacy against infection (1-hazard ratio)
 VE_s <- 0.7
 # vaccine efficacy against progression to symptoms given infection
-VE_p <- 0.4
+VE_p_vec <- seq(0, 0.9, 0.01)
 # vaccine efficacy against progression to severe disease given symptoms
-VE_h <- 0.2
+VE_h_vec <- seq(0, 0.9, 0.01)
 
-# true value of VE_sp
-VE_sp <- 0.82
-# true value of VE_sph
-VE_sph <- 0.856
+################################################################################
 
 # percent of population that is older
 pct_older <- 0.5
 # hazard ratio relating older age and testing positive
 hr_older_tp <- 1.0
 # hazard ratio relating older age and testing negative
-hr_older_tn <- 2.0
-
-# can further add another layer that allows different vaccination probability
-
-################################################################################
-
-# define hazard function for infection with target pathogen in reference group
+hr_older_tn <- 2.0# define hazard function for infection with target pathogen in reference group
 # start out with a weibull
 shape_tp <- 1.2
 rate_tp0 <- 0.01
@@ -91,8 +78,6 @@ prob_test_2_unvax <- 0.7
 # probability that an unvaccinated person with severe symptoms seeks testing
 prob_test_3_unvax <- 0.9
 
-## can further modify this to depend on age
-
 ################################################################################
 
 # run the simulation
@@ -100,10 +85,13 @@ set.seed(12345)
 
 # create dataframe for bias output
 cols <- c("est_VE_s", "est_VE_sp", "est_VE_sph")
-VE_df <- data.frame(matrix(ncol=length(cols),nrow=0))
-colnames(VE_df) <- cols
+VE_p_df <- data.frame(matrix(ncol=length(cols),nrow=0))
+colnames(VE_p_df) <- cols
 
-for(i in 1:num_runs){
+# changing VE_p
+for(i in 1:length(VE_p_vec)){
+  VE_p <- VE_p_vec[i]
+  VE_h <- 0.2
   source("Functions.R")
   
   df_keep <- df[df$keep==1,]
@@ -124,59 +112,72 @@ for(i in 1:num_runs){
   logfit_sph <- glm(result ~ vax + age + s(time,3), data=df_sph, family=binomial)
   est_VE_sph <- 1-exp(logfit_sph$coefficients[2])
   
-  VE_df <- VE_df %>% add_row(est_VE_s = est_VE_s, est_VE_sp = est_VE_sp, 
+  VE_p_df <- VE_p_df %>% add_row(est_VE_s = est_VE_s, est_VE_sp = est_VE_sp, 
                              est_VE_sph = est_VE_sph)
 }
 
-view(VE_df)
+saveRDS(VE_p_df, file = "estimates_from_changing_VE_p")
 
-### Results
-cols <- c("avg_est_VE_s", "avg_est_VE_sp", "avg_est_VE_sph",
-          "mean_bias_s", "mean_bias_sp", "mean_bias_sph", 
-          "mcse_bias_s", "mcse_bias_sp", "mcse_bias_sph",
-          "root_mse_s", "root_mse_sp", "root_mse_sph")
-results_df <- data.frame(matrix(ncol=length(cols),nrow=0))
-colnames(results_df) <- cols
+# jpeg(file = "VE_s vs VE_p.jpeg")
+# plot(VE_p_vec, VE_p_df$est_VE_s, main = "VE_s estimate vs. VE_p",
+#      xlab = "VE_p", ylab = "VE_s estimate")
+# dev.off()
+# 
+# jpeg(file = "VE_sp vs VE_p.jpeg")
+# plot(VE_p_vec, VE_p_df$est_VE_sp, main = "VE_sp estimate vs. VE_p",
+#      xlab = "VE_p", ylab = "VE_sp estimate")
+# dev.off()
+# 
+# jpeg(file = "VE_sph vs VE_p.jpeg")
+# plot(VE_p_vec, VE_p_df$est_VE_sph, main = "VE_sph estimate vs. VE_p",
+#      xlab = "VE_p", ylab = "VE_sph estimate")
+# dev.off()
 
-# Averages
-avg_est_VE_s <- mean(est_VE_s)
-avg_est_VE_sp <- mean(est_VE_sp)
-avg_est_VE_sph <- mean(est_VE_sph)
+# changing VE_h
+cols <- c("est_VE_s", "est_VE_sp", "est_VE_sph")
+VE_h_df <- data.frame(matrix(ncol=length(cols),nrow=0))
+colnames(VE_h_df) <- cols
 
-# Calculating mean biases
-mean_bias_s <- mean(VE_df$est_VE_s - VE_s)
-mean_bias_sp <- mean(VE_df$est_VE_sp - VE_sp)
-mean_bias_sph <- mean(VE_df$est_VE_sph - VE_sph)
+for(i in 1:length(VE_h_vec)){
+  VE_h <- VE_h_vec[i]
+  VE_p <- 0.2
+  source("Functions.R")
+  
+  df_keep <- df[df$keep==1,]
+  df_s <- df_keep
+  df_sp <- df_keep[df_keep$severity==2 | df_keep$severity==3,]
+  df_sph <- df_keep[df_keep$severity==3,]
+  
+  # fit logistic regression model adjusting for age and calendar time
+  # finding VE_s
+  logfit_s <- glm(result ~ vax + age + s(time,3), data=df_s, family=binomial)
+  est_VE_s <- 1-exp(logfit_s$coefficients[2])
+  
+  # finding VE_sp
+  logfit_sp <- glm(result ~ vax + age + s(time,3), data=df_sp, family=binomial)
+  est_VE_sp <- 1-exp(logfit_sp$coefficients[2])
+  
+  # finding VE_sph
+  logfit_sph <- glm(result ~ vax + age + s(time,3), data=df_sph, family=binomial)
+  est_VE_sph <- 1-exp(logfit_sph$coefficients[2])
+  
+  VE_h_df <- VE_h_df %>% add_row(est_VE_s = est_VE_s, est_VE_sp = est_VE_sp, 
+                             est_VE_sph = est_VE_sph)
+}
 
-# Monte Carlo Standard Error of Bias
-mcse_bias_s <- sqrt((1/(num_runs * (num_runs - 1))) * sum((VE_df$est_VE_s - VE_s)^2))
-mcse_bias_sp <- sqrt((1/(num_runs * (num_runs - 1))) * sum((VE_df$est_VE_sp - VE_sp)^2))
-mcse_bias_sph <- sqrt((1/(num_runs * (num_runs - 1))) * sum((VE_df$est_VE_sph - VE_sph)^2))
+saveRDS(VE_h_df, file = "estimates_from_changing_VE_h")
 
-# Calculating Square Root MSE
-root_MSE_s <- sqrt(mean((VE_s - VE_df$est_VE_s)^2))
-root_MSE_sp <- sqrt(mean((VE_sp - VE_df$est_VE_sp)^2))
-root_MSE_sph <- sqrt(mean((VE_sph - VE_df$est_VE_sph)^2))
-
-# Mean Absolute Error
-#MAE_s <- mae(VE_df$est_VE_s, 0.85)
-#MAE_sp <- mae(VE_df$est_VE_sp, 0.85)
-#MAE_sph <- mae(0.85, VE_df$est_VE_sph)
-
-results_df <- results_df %>% add_row(avg_est_VE_s = avg_est_VE_s, 
-                                     avg_est_VE_sp = avg_est_VE_sp, 
-                                     avg_est_VE_sph = avg_est_VE_sph,
-                                     mean_bias_s = mean_bias_s, 
-                                     mean_bias_sp = mean_bias_sp, 
-                                     mean_bias_sph = mean_bias_sph, 
-                                     mcse_bias_s = mcse_bias_s, 
-                                     mcse_bias_sp = mcse_bias_sp, 
-                                     mcse_bias_sph = mcse_bias_sph,
-                                     root_mse_s = root_MSE_s, 
-                                     root_mse_sp = root_MSE_sp, 
-                                     root_mse_sph = root_MSE_sph)
-view(results_df)
-
-# Monte Carlo Standard Error
-# average standard error 
-
+# jpeg(file = "VE_s vs VE_h.jpeg")
+# plot(VE_p_vec, VE_p_df$est_VE_s, main = "VE_s estimate vs. VE_h",
+#      xlab = "VE_h", ylab = "VE_s estimate")
+# dev.off()
+# 
+# jpeg(file = "VE_sp vs VE_h.jpeg")
+# plot(VE_p_vec, VE_p_df$est_VE_sp, main = "VE_sp estimate vs. VE_h",
+#      xlab = "VE_h", ylab = "VE_sp estimate")
+# dev.off()
+# 
+# jpeg(file = "VE_sph vs VE_h.jpeg")
+# plot(VE_p_vec, VE_p_df$est_VE_sph, main = "VE_sph estimate vs. VE_h",
+#      xlab = "VE_h", ylab = "VE_sph estimate")
+# dev.off()
